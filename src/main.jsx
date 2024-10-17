95aea6ea435092240da71b909577a2546e7e3386
Syed Saleem Raza Shah <syedsaleemrazashah69@gmail.com>
2024-10-17 22:19:28 +0500

    react

---
+++ src/main.jsx
@@ -0,0 +1,8 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <h1>Saleem Raza</h1>
+  </StrictMode>
+);
